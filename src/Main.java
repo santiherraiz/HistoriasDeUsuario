6d222dca3b3ee442ac6976be8ef5104c32328be8
Carlos <carferher8@alu.edu.gva.es>
2025-12-10 14:46:53 +0100

    Leer y Escribir CSV implementado en funciones

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,5 +17,64 @@ public class Main {
         System.out.println("2. Test conectividad");
         System.out.println("3. Salir");
         System.out.println("=================");
+    }
+
+    public static ArrayList<Datos> leerCsv(){
+        ArrayList<Datos> datos = new ArrayList<>();
+
+        try{
+            File file = new File("db.csv");
+
+            if (!file.exists()) {
+                BufferedWriter bw = new BufferedWriter(new FileWriter("db.csv"));
+                bw.write("id,titulo,estado");
+                bw.close();
+            }
+
+            BufferedWriter bw2 = new BufferedWriter(new FileWriter("db.csv", true));
+
+            datos.forEach(d -> {
+                try {
+                    bw2.newLine();
+                    bw2.write(d.toString());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return datos;
+    }
+
+    public static void escribirCSV(ArrayList<Datos> datos){
+        try{
+            File file = new File("db.csv");
+
+            if (file.exists()){
+                BufferedReader br = new BufferedReader(new FileReader(file));
+
+                br.readLine();
+
+                String linea;
+                while ((linea = br.readLine()) != null){
+                    Estado estado = Estado.ABIERTO;
+                    if (!estado.equals("ABIERTO")){
+                        estado = Estado.CERRADO;
+                    }
+
+                    datos.add(new Datos(linea.split(",")[1],estado));
+                }
+
+            } else {
+                System.out.println("No existe");
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
