e72d70cc5cf01c592a522c9b795cd5c8528ef351
Carlos <carferher8@alu.edu.gva.es>
2025-12-10 14:10:50 +0100

    Leer CSV

--- src/Datos.java
+++ src/Datos.java
@@ -1,4 +1,17 @@
 public class Datos {
     private int id;
     private String titulo;
+    private boolean estado;
+
+
+    public Datos(int id, String titulo, boolean estado) {
+        this.id = id;
+        this.titulo = titulo;
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return id+","+titulo+","+estado;
+    }
 }
